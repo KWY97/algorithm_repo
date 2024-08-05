cb6f3abaaee458583866ce865a0bd0b866905b0c
kwy97 <skajtwlkt@naver.com>
2024-08-05 16:13:20 +0900

    240805

--- 240805_String_1_2/inclass/string_1_2.py
+++ 240805_String_1_2/inclass/string_1_2.py
@@ -1,28 +1,19 @@
 # 1. 고지식한 패턴 검색 알고리즘
 
-# while문을 사용한 식
 # p = 찾을 패턴
 # M = len(p)
 # t = 전체 텍스트
 # N = len(t)
 
-while i < N and j < M:
-    if t[i] == p[j]: # 일치하는 경우
-        i += 1
-        j += 1
-    else: # 불일치하는 경우
-        i = i - j + 1
-        j = 0
-
-# for문을 사용한 예시 문제
+# for문을 사용한 예시
 p = 'TTA'
-M = len(p)
+M = len(p) # 3
 t = 'TTTTTABC'
-N = len(t)
+N = len(t) # 8
 cnt = 0
 
 for i in range(N-M+1): # 비교 시작 위치
-    for j in range(M):
+    for j in range(M): # 0 1 2
         if t[i + j] != p[j]:
             break # for j문을 break, 다음 글자부터 비교 시작
     else: # break 걸리지 않고 for j 문이 중단없이 반복되면
@@ -30,16 +21,18 @@ for i in range(N-M+1): # 비교 시작 위치
 print(cnt)
 
 
+# 패턴과 일치하는 구간이 있으면 구간의 시작 인덱스 리턴, 없으면 -1 리턴하는 함수
+def BruteForce_1(p, t):
+    N = len(t)
+    M = len(p)
+
+    for i in range(N - M + 1):  # 비교 시작 위치
+        for j in range(M):
+            if t[i + j] != p[j]:
+                break  # for j문을 break, 다음 글자부터 비교 시작
+        else:  # break 걸리지 않고 for j 문이 중단없이 반복되면
+            return i  # 패턴을 찾은 경우
+    return -1 # 패턴을 못 찾은 경우
 
 
 
-
-
-
-
-
-
-
-
-
-# 2. KMP 알고리즘
--- 240805_String_1_2/ws/1213_string_cnt.py
+++ 240805_String_1_2/ws/1213_string_cnt.py
@@ -2,5 +2,24 @@ T = 10
 
 for _ in range(T):
     tc = int(input())
-    search_str = input() # 찾을 문자열
-    standard_str = input() # 기준 문자열
+    p = input() # 찾을 문자열
+    t = input() # 기준 문자열
+    cnt = 0
+    N = len(t)
+    M = len(p)
+
+    for i in range(N-M+1): # 비교 시작 위치
+        for j in range(M): # 비교할 문자열의 개수만큼
+            if t[i+j] != p[j]:
+                break # for j문을 빠져나옴, 다음 위치부터 다시 비교 시작
+        else: # break 안걸리고 통과했다면
+            cnt += 1
+
+    print(f'#{tc} {cnt}')
+
+
+for i in range(N-M+1):
+    for j in range(M):
+        if t[i+j] != p[j]:
+            break
+
