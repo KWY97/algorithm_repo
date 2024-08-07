75b52da9412fc9375dbf0369e21077000e2d8fb9
kwy97 <skajtwlkt@naver.com>
2024-08-07 17:50:51 +0900

    240807

--- 240806_Stack_1_1/hw/pascal_triangle.py
+++ 240806_Stack_1_1/hw/pascal_triangle.py
@@ -1,26 +1,17 @@
-def pascal(N):
+def pascal(row, col):
     '''
-    줄의 개수는 N줄
-    끝과 끝은 1
-    i번째 줄의 원소는 i개
+    인덱스에 위치하는 값을 return 해주는 함수
     '''
-    for i in range(1, N+1): # 줄
-        for j in range(1, i+1): # 원소의 개수만큼 반복
-
-
-
-
-
-
-
-
+    if col == 0 or col == row:
+        return 1
+    return pascal(row-1, col-1) + pascal(row-1, col)
 
 T = int(input())
-
 for tc in range(1, T+1):
-    N = int(input()) # 파스칼 삼각형의 크기
-    arr = [[0] * N for _ in range(N)]
-
-    # ans = pascal(N)
-    # print(f'#{tc}')
-    # print(ans)
+    N = int(input())
+    print(f'#{tc}')
+    for i in range(N):
+        v_list = []
+        for j in range(i+1):
+            v_list.append(pascal(i,j))
+        print(*v_list)
