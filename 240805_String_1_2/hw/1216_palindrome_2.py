234b1225baa13b21d477c47b1b29e22e4ced6953
kwy97 <kwy1547@gmail.com>
2024-08-11 23:15:23 +0900

    240811

--- 240805_String_1_2/hw/1216_palindrome_2.py
+++ 240805_String_1_2/hw/1216_palindrome_2.py
@@ -1,23 +1,33 @@
-T = 10
-N = 100
+def is_palidrome(string):
+    left, right = 0, len(string) - 1
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
 
+def palindrome(arr): # 배열, x좌표, y좌표
+    for i in range(N): # 기준 행을 i로
+        for length in range(N, 1, -1): # 긴 문자열 먼저 검사를 시행
+            for start in range(N - length + 1): # 윈도우 시작 인덱스
+                # 가로 방향 검사
+                if is_palidrome(arr[i][start:start+length]):
+                    return length
+
+                # 세로 방향 검사
+                column = []
+                for j in range(start, start + length):
+                    column.append(arr[j][i])
+                if is_palidrome(column):
+                    return length
+    return 1
+
+T = 10
 for _ in range(T):
     tc = int(input())
+    N = 100
     arr = [list(input()) for _ in range(N)]
 
-'''
-<생각 정리>
-arr = ['A', 'B', 'B', 'A', 'C', 'C', 'A', 'B', 'A']
-N = len(arr)
-v_list = ['A', 'B', 'C']
-idx_list = [] # A, B, C의 인덱스를 [], [], []로 받을 바깥 리스트
-
-[[0,3,6,8], [1,2,7], [4,5]]
-
-for x in v_list:
-    temp_list = [] # A, B, C의 인덱스를 담을 리스트
-    for i in range(N):
-        if arr[i] == x:
-            temp_list.append(i)
-    idx_list.append(temp_list)
-'''
+    ans = palindrome(arr)
+    print(f'#{tc} {ans}')
--- practice.py
+++ practice.py
@@ -1,15 +1,8 @@
-def f(i, N, v):
-    if i == N:
-        return 0
-    elif arr[i] == v:
-        return 1
-    else:
-        return f(i+1, N, v)
-    
-
-
-arr = [1, 2, 3, 4, 5]
-N = len(arr)
-
-print(f(0, N, 3))
-
+def is_palidrome(string):
+    left, right = 0, len(string) - 1
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
