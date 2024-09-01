2796f71514a5b1428c96da70f93d9a286ebb9776
kwy97 <kwy1547@gmail.com>
2024-09-02 01:43:04 +0900

    240901

--- 240828_Tree_1_2/hw/1232_ four_basic_operations.py
+++ 240828_Tree_1_2/hw/1232_ four_basic_operations.py
@@ -1,23 +1,4 @@
-def cal(left, right, operator):
-    if operator == '+':
-        ans = left + right
-    elif operator == '-':
-        ans = left - right
-    elif operator == '*':
-        ans = left * right
-    else:
-        ans = left / right
-    return int(ans)
-
-def postorder(n):
-    if type(arr[n][1]) == type(''):
-        left = postorder(arr[n][2])
-        right = postorder(arr[n][3])
-        return cal(left, right, arr[n][1])
-    else:
-        return arr[n][1]
-
 for tc in range(1, 11):
     N = int(input())
-    arr = [0] + [list(map(lambda x: int(x) if x.isdecimal() else x, input().split())) for _ in range(N)]
-    print(f'#{tc} {postorder(1)}')
+    arr = [list(map(lambda x: int(x) if x.isdecimal() else x, input().split())) for _ in range(N)]
+    print(arr)
