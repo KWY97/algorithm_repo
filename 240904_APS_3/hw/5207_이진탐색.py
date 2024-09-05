a7dae97171de6f1ca0d3fe1d8b156f8eaec4bff0
unknown <kwy1547@gmail.com>
2024-09-05 12:49:08 +0900

    240905

--- 240904_APS_3/hw/5207_이진탐색.py
+++ 240904_APS_3/hw/5207_이진탐색.py
@@ -1,26 +1,26 @@
 def binary_search(target):
     low = 0
     high = len(A) - 1
-    cnt = 0 # 조건을 만족하는 숫자의 개수
-
-    # 한 번에 찾았을 시
-    mid = (low + high) // 2
-    if A[mid] == target:
-        return 1 # 1개 반환
-
-    elif A[mid] > target: # 중간값 보다 타겟 값이 작을시 끝점을 중간값 한 칸 앞으로
-        high = mid - 1
-        flag = 0
-
-    else: # 중간값 보다 타겟 값이 클시 시작점을 중간값 한 칸 뒤로
-        low = mid + 1
-        flag = 1
+    flag = [] # -1(좌), 1(우) 기록
 
     while low <= high:
-        mid = (low + high) // 2 # 새롭게 중간값 갱신
+        mid = (low + high) // 2
 
-        if flag == 0: # 직전에 왼쪽을 탐색 했다면
-            if mid < target:
+        if A[mid] == target:
+            return 1  # 1개 반환
+
+        elif A[mid] > target:  # 중간값 보다 타겟 값이 작을시 끝점을 중간값 한 칸 앞으로
+            high = mid - 1
+            flag.append(-1)
+
+        else:  # 중간값 보다 타겟 값이 클시 시작점을 중간값 한 칸 뒤로
+            low = mid + 1
+            flag.append(1)
+
+        if len(flag) >= 2:
+            for i in range(len(flag) - 1):
+                if flag[i] == flag[i+1]:
+                    return 0
 
 T = int(input())
 for tc in range(1, T+1):
@@ -38,8 +38,4 @@ for tc in range(1, T+1):
 
     for v in nums:
         ans += binary_search(v)
-    print(ans)
-
-
-
-
+    print(f'#{tc} {ans}')
