85d243f9591b9306f4099852c713099db76b6d18
Yu-jiin <jiin4083@naver.com>
2024-09-06 17:44:09 +0900

    240906

---
+++ practice.py
@@ -0,0 +1,11 @@
+# 세로읽기
+#for idx in range(N):
+#     for jdx in range(M):
+#
+#
+# for jdx in range(각 행의 길이중에서 최대 길이):
+#     for idx in range(N):
+#         if len(grid[idx]) <= jdx: continue
+
+size = 5
+arr = [list(input()) for _ in range(size)]
