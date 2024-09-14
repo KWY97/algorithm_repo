3c51e1c64d0225302bcce0e910c7dfe01a82b9d4
kwy97 <kwy1547@gmail.com>
2024-09-15 00:38:48 +0900

    240915

--- practice.py
+++ practice.py
@@ -1,8 +1,12 @@
+# 백준 1193번 하는중
 N = int(input())
-max_num = 1
+bunja = 1
+bunmo = 1
+line = 1
 cnt = 1
 
-while N > max_num:
-    max_num = max_num + (6 * cnt)
-    cnt += 1
-print(cnt)
+while N > line:
+    
+    line += 1
+print(line)
+
